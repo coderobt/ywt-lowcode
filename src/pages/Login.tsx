ae6058bee47391d9569452380b4ea361ccaa4937
ywt <Yinwt100@qq.com>
2024-04-28 13:59:05 +0800

    feat: 登录form的构建

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,83 @@
-import React, { FC } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { FC, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Typography, Space, Form, Input, Button, Checkbox } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import styles from './Login.module.scss'
+import { REGISTER_PATHNAME } from '@/router/index'
+
+const { Title } = Typography
+
+const USERNAME_KEY = 'USERNAME'
+const PASSWORD_KEY = 'PASSWORD'
+
+function rememberUser(username: string, password: string) {
+  localStorage.setItem(USERNAME_KEY, username)
+  localStorage.setItem(PASSWORD_KEY, password)
+}
+function deleteUserFromStorage() {
+  localStorage.removeItem(USERNAME_KEY)
+  localStorage.removeItem(PASSWORD_KEY)
+}
+function getUserInfoFromStorage() {
+  return {
+    username: localStorage.getItem(USERNAME_KEY),
+    password: localStorage.getItem(PASSWORD_KEY),
+  }
+}
 
 const Login: FC = () => {
-  const navigate = useNavigate()
+  // const nav = useNavigate()
+  const [form] = Form.useForm() //第三方hook
+  useEffect(() => {
+    const { username, password } = getUserInfoFromStorage()
+    form.setFieldsValue({ username, password })
+  }, [])
+  const onFinish = (values: any) => {
+    // console.log(values)
+    const { username, password, remember } = values || {}
+    if (remember) {
+      rememberUser(username, password)
+    } else {
+      deleteUserFromStorage()
+    }
+  }
 
   return (
-    <div>
-      <p>Login</p>
+    <div className={styles.container}>
       <div>
-        <button onClick={() => navigate(-1)}>返回</button>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>登录</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          form={form}
+        >
+          <Form.Item label="用户名" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码" name="password">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 6, span: 16 }}>
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                登录
+              </Button>
+              <Link to={REGISTER_PATHNAME}>注册新用户</Link>
+            </Space>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   )
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,52 @@
 import React, { FC } from 'react'
+import { Typography, Space, Form, Input, Button } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import styles from './Register.module.scss'
+import { LOGIN_PATHNAME } from '@/router/index'
+
+const { Title } = Typography
 
 const Register: FC = () => {
-  return <p>register</p>
+  const onFinish = (values: any) => {
+    console.log(values)
+  }
+  return (
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>注册新用户</Title>
+        </Space>
+      </div>
+      <div>
+        <Form onFinish={onFinish} labelCol={{ span: 6 }} wrapperCol={{ span: 16 }}>
+          <Form.Item label="用户名" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码" name="password">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item label="确认密码" name="confirm">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item label="昵称" name="nickname">
+            <Input />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                注册
+              </Button>
+              <Link to={LOGIN_PATHNAME}>已有账户，登录</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
 }
 
 export default Register
