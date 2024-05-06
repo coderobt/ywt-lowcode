b247d296317485d94a16f285d8d25e96ae263e76
ywt <Yinwt100@qq.com>
2024-05-06 09:23:01 +0800

    feat: 保存问卷信息,判断loading防止连续保存

--- src/pages/question/Edit/components/EditHeader/index.tsx
+++ src/pages/question/Edit/components/EditHeader/index.tsx
@@ -1,12 +1,15 @@
 import React, { FC, useState, ChangeEvent } from 'react'
 import styles from './index.module.scss'
 import { Button, Typography, Space, Input } from 'antd'
-import { LeftOutlined, EditOutlined } from '@ant-design/icons'
-import { useNavigate } from 'react-router-dom'
+import { LeftOutlined, EditOutlined, LoadingOutlined } from '@ant-design/icons'
+import { useNavigate, useParams } from 'react-router-dom'
 import EditToolbar from '../EditToolbar'
 import useGetPageInfo from '@/hooks/useGetPageInfo'
+import useGetComponentInfo from '@/hooks/useGetComponentInfo'
 import { useDispatch } from 'react-redux'
 import { changePageTitle } from '@/store/pageInfoReducer'
+import { updateQuestionService } from '@/services/question'
+import { useRequest, useKeyPress } from 'ahooks'
 
 const { Title } = Typography
 
@@ -50,6 +53,36 @@ const TitleElem: FC = () => {
   )
 }
 
+// 保存按钮
+const SaveButton: FC = () => {
+  //pageInfo componentList
+  const { id } = useParams()
+  const { componentList = [] } = useGetComponentInfo()
+  const pageInfo = useGetPageInfo()
+
+  const { loading, run: save } = useRequest(
+    async () => {
+      if (!id) return
+      await updateQuestionService(id, { ...pageInfo, componentList })
+    },
+    { manual: true },
+  )
+
+  //快捷键保存
+  useKeyPress(['ctrl.s', 'meta.s'], (event: KeyboardEvent) => {
+    //禁用默认行为
+    event.preventDefault()
+    if (!loading) save()
+  })
+
+  return (
+    //给按钮加loading 防止重复点击
+    <Button onClick={save} disabled={loading} icon={loading ? <LoadingOutlined /> : null}>
+      保存
+    </Button>
+  )
+}
+
 // 编辑器头部
 const EditHeader: FC = () => {
   const nav = useNavigate()
@@ -69,7 +102,7 @@ const EditHeader: FC = () => {
         </div>
         <div className={styles.right}>
           <Space>
-            <Button>保存</Button>
+            <SaveButton />
             <Button type="primary">发布</Button>
           </Space>
         </div>
